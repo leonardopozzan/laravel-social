b8bd32f77bdeb9abc1c03670afb6ba9e1e288e99
ValentinaZussino <vzussino@protonmail.com>
2023-01-02 18:43:24 +0100

    controller completo + prova colleg index

--- app/Http/Controllers/Guest/ProductController.php
+++ app/Http/Controllers/Guest/ProductController.php
@@ -4,27 +4,29 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     *
      */
     public function index()
     {
-        //
+        $products = Product::all();
+        return view('products.index', compact('products'));
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     *
      */
     public function create()
     {
-        //
+        return view('products.create');
     }
 
     /**
@@ -33,43 +35,49 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+    
     public function store(Request $request)
     {
-        //
+
+        $form_data = $request->all();
+
+        //dd($form_data);
+        $new_product = Product::create($form_data);
+        return redirect()->route('products.show', $new_product->id);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    
+    public function show(Product $product)
     {
-        //
+        return view('products.show', compact('product'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     *
      */
-    public function edit($id)
+    public function edit(Product $product)
     {
-        //
+        return view('products.edit', compact('product'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * 
      */
     public function update(Request $request, $id)
     {
-        //
+        $product = Product::find($id);
+        $form_data = $request->all();
+        $product->title = $form_data['title'];
+        $product->price = $form_data['price'];
+        $product->description = $form_data['description'];
+        $product->status = $form_data['status'];
+        $product->available = $form_data['available'];
+        $product->update();
+        return redirect()->route('products.show', $product->id);
     }
 
     /**
@@ -78,8 +86,9 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return redirect()->route('products.index');
     }
 }
