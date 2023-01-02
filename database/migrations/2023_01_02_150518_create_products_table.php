542c559169a5945480ec8bbb21d0a23f30a9d701
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2023-01-02 17:47:40 +0100

    creato e popolato database

--- database/migrations/2023_01_02_150518_create_products_table.php
+++ database/migrations/2023_01_02_150518_create_products_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->date('subscribed_at');
-            $table->rememberToken();
+            $table->string('title', 100);
+            $table->float('price', 7, 2);
+            $table->text('description')->nullable();
+            $table->string('status', 20);
+            $table->boolean('available');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
