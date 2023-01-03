54893d2a968709d2542c94f66bc07ce2fb6efe4d
AlessioBonadonna <alessiobonadonna23@gmail.com>
2023-01-03 16:00:11 +0100

    fix graphic

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,42 @@
 <header>
-    @if(Auth::check())
-    Benvenuto <b>{{ Auth::user()->name }}</b> <a href="{{ route('logout') }}">Logout</a>
-    @else
-    <a href="{{ route('login') }}">Login</a>
-    @endif
+    <!-- Image and text -->
+    <nav class="navbar  ">
+        <div class="container">
+
+
+            <div class="img-box-header">
+
+                <a class="navbar-brand" href="{{ route('home') }}">
+                    <img src="{{ Vite::asset('resources/img/logo_ecommerce_bianco.png') }}"
+                        class="d-inline-block align-top" alt="logo-site">
+                </a>
+            </div>
+            <div>
+                <ul class="d-flex  list-unstyled">
+                    <li class="px-3 "> <a href="{{ route('products.index') }}"> Shop</a></li>
+                    <li class="px-3"> <a href="#nogo">Il nostro team </a> </li>
+                    @if (Auth::check())
+                        <li class="my-3"> <a href="{{ route('products.create') }}">crea un prodotto </a></li>
+                    @endif
+
+
+                </ul>
+            </div>
+
+
+            <div>
+
+                @if (Auth::check())
+                    Benvenuto <a href="#nogo">{{ Auth::user()->name }}</a> <a href="{{ route('logout') }}"> <button
+                            class="my-btn">Logout </button> </a>
+                @else
+                    <a href="{{ route('login') }}"> <button class="my-btn">
+                            Login
+                        </button> </a>
+                @endif
+            </div>
+        </div>
+    </nav>
+
+
 </header>
