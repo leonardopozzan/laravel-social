b6979f55635d86f249f1f0c230c0a1472cbf1342
AlessioBonadonna <alessiobonadonna23@gmail.com>
2023-01-05 18:05:45 +0100

    change hover card and media quary

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,20 +1,22 @@
 @extends('layout.app')
-<?php 
+<?php
 $search = '';
-if(isset($_GET['search'])){
+if (isset($_GET['search'])) {
     $search = $_GET['search'];
 }
 ?>
 @section('content')
     <div id="index">
-        <h1>elenco prodotti</h1>
+
         <div class="container d-flex">
-            <form class="" action="{{route('products.index')}}" method="GET">
+            <form class="my-3" action="{{ route('products.index') }}" method="GET">
                 <select name="search" id="search">
-                    <option value="" {{($search == '') ? 'selected' : ''}}>Vedi tutti i prodotti</option>
-                    <option value="nuovo"  {{($search == 'nuovo') ? 'selected' : ''}}>Prodotti nuovi</option>
-                    <option value="usato buone condizioni"  {{($search == 'usato buone condizioni') ? 'selected' : ''}}>Prodotti in buone condizioni</option>
-                    <option value="usato come nuovo"  {{($search == 'usato come nuovo') ? 'selected' : ''}}>Prodotti come nuovi</option>
+                    <option value="" {{ $search == '' ? 'selected' : '' }}>Vedi tutti i prodotti</option>
+                    <option value="nuovo" {{ $search == 'nuovo' ? 'selected' : '' }}>Prodotti nuovi</option>
+                    <option value="usato buone condizioni" {{ $search == 'usato buone condizioni' ? 'selected' : '' }}>
+                        Prodotti in buone condizioni</option>
+                    <option value="usato come nuovo" {{ $search == 'usato come nuovo' ? 'selected' : '' }}>Prodotti come
+                        nuovi</option>
                 </select>
                 <button type="submit" class="my-btn ms-3">Cerca</button>
             </form>
@@ -22,35 +24,36 @@ if(isset($_GET['search'])){
         <section class="container">
             <div class="cards-container">
                 @foreach ($products as $product)
-                {{-- card --}}
-                    <div class="card my-3 single-card" >
+                    {{-- card --}}
+                    <div class="card my-3 single-card ">
                         {{-- card img product --}}
-                        <a href="{{route('products.show', $product->id)}}">
+                        <a href="{{ route('products.show', $product->id) }}">
                             <div class="img-product">
-                            {{-- <img src={{$product->image}} class="card-img-top" alt="..."> --}}
+                                <img src="https://picsum.photos/id/{{ $product->id + 10 }}/1920/1080"
+                                    alt="{{ $product->title }}">
+
                             </div>
                         </a>
                         {{-- card body text --}}
                         <div class="card-body">
                             <div class="card-text-overview">
                                 <div class="p-3">
-                                    <h3 class="">{{$product->title}}</h3>
-                                    <p class="">{{$product->description}}</p>
+                                    <h3 class="">{{ $product->title }}</h3>
+                                    <p class="">{{ $product->description }}</p>
                                 </div>
                             </div>
                             <div class="card-details">
                                 <div class="card-price ">
-                                    <p class="pt-3"> <span>Price:</span> {{$product->price}} £</p>
+                                    <p class="pt-3"> <span>Price:</span> {{ $product->price }} €</p>
                                 </div>
                                 <div class="card-status">
-                                    <p class="pt-3"><span>Condizioni:</span> <br> {{$product->status}}</p>
+                                    <p class="pt-3"><span>Condizioni:</span> <br> {{ $product->status }}</p>
                                 </div>
                             </div>
                         </div>
                     </div>
-            @endforeach 
-            </div>  
+                @endforeach
+            </div>
         </section>
     </div>
-    
 @endsection
