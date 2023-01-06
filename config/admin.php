767b08042a4e46977421f2ff738fe3c602a9e360
ValentinaZussino <vzussino@protonmail.com>
2023-01-06 14:35:27 +0100

    impostazione cards team page

--- config/admin.php
+++ config/admin.php
@@ -4,26 +4,42 @@ return [
         'name' => 'leonardo',
         'email' => 'leonardo@buymore.com',
         'password' => 'pozzan',
+        'image' => 'https://avatars.githubusercontent.com/u/108693204?v=4',
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, tempora enim pariatur saepe illo impedit?',
+        'qualification' => 'CEO, Full Stack Developer'
     ],
     [
         'name' => 'valentina',
         'email' => 'valentina@buymore.com',
         'password' => 'zussino',
+        'image' => 'https://avatars.githubusercontent.com/u/107954392?v=4
+        ',
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, tempora enim pariatur saepe illo impedit?',
+        'qualification' => 'COO, Full Stack Developer'
     ],
     [
         'name' => 'alessio',
         'email' => 'alessio@buymore.com',
         'password' => 'bonadonna',
+        'image' => 'https://avatars.githubusercontent.com/u/48909804?v=4',
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, tempora enim pariatur saepe illo impedit?',
+        'qualification' => 'Project Manager, Full Stack Developer'
     ],
     [
         'name' => 'lorenzo',
         'email' => 'lorenzo@buymore.com',
         'password' => 'castiglion',
+        'image' => 'https://avatars.githubusercontent.com/u/111997828?v=4',
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, tempora enim pariatur saepe illo impedit?',
+        'qualification' => 'Chief Designer, Full Stack Developer'
     ],
     [
         'name' => 'riccardo',
         'email' => 'riccardo@buymore.com',
         'password' => 'pellegrino',
+        'image' => 'https://avatars.githubusercontent.com/u/64532705?v=4',
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, tempora enim pariatur saepe illo impedit?',
+        'qualification' => 'CTO, Full Stack Developer'
     ],
 ]
 ?>
--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -1,4 +1,39 @@
+<?php
+$users = config('admin')
+?>
+
 @extends('layout.app')
+
 @section('content')
-    SONO LA PAGINA PROFILO XO XO
+    <div  id="team">
+        <section class="container">
+            {{-- cards container --}}
+            <div class="cards-container">
+                @foreach ($users as $user)
+                {{-- card --}}
+                <div class="card my-4 single-card">
+                    {{-- img --}}
+                    <div class="pt-4 pb-3 d-flex justify-content-center align-items-center">
+                        <img class="img-profile" src="{{$user['image']}}" alt="immagine profilo {{$user['name']}}">
+                    </div>
+                    {{-- body --}}
+                    <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                        {{-- person --}}
+                        <div class="profile-person">
+                            <span class="profile-name ">{{$user['name']}}</span>
+                            <span class="profile-qualification ">{{$user['qualification']}}</span>
+                            <p class="profile-description">{{$user['description']}}</p>
+                        </div>
+                        {{-- social --}}
+                        <div class="profile-social">
+                            <i class="fa-brands fa-facebook"></i>
+                            <i class="fa-brands fa-instagram"></i>
+                            <i class="fa-brands fa-telegram"></i>
+                        </div>
+                    </div>
+                </div>  
+                @endforeach
+            </div>
+        </section>
+    </div>
 @endsection
