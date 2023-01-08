304e3917c3e4354c7f255941461f29a518909b15
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2023-01-08 19:05:42 +0100

    stile regiter-form

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,20 +1,25 @@
 @extends('layout.app')
 @section('content')
-    <div class="container crea">
+    <div class="crea d-flex align-items-center justify-content-center">
         <div class="p-5">
             <div class="my-6 bg-create p-5">
                 <h1>CREA UN ACCOUNT </h1>
-                <form action="{{ route('users.store') }}" method="POST" class="">
+                <form action="{{ route('users.store') }}" method="POST" id="register-form">
                     @csrf
                     {{-- name email password --}}
-                    <label for="name">Nome</label>
-                    <input type="text" name="name">
-                    <label for="email">Email</label>
-                    <input type="text" name="email">
-                    <label for="password">Password</label>
-                    <input type="password" name="password" id="">
-
-                    <button>Registrati</button>
+                    <div class="field">
+                        <label for="name">Nome</label>
+                        <input type="text" name="name">
+                    </div>
+                    <div class="field">
+                        <label for="email">Email</label>
+                        <input type="text" name="email">
+                    </div>
+                    <div class="field">
+                        <label for="password">Password</label>
+                        <input type="password" name="password">
+                    </div>
+                    <button type="submit" class="my-btn">Registrati</button>
                 </form>
             </div>
         </div>
