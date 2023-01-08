51528a05665a2043810541d0a36daeb261217998
AlessioBonadonna <alessiobonadonna23@gmail.com>
2023-01-08 16:43:29 +0100

    responsive show

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -13,18 +13,22 @@
 
 
             {{-- img prodotto --}}
-            <div class="col-6 me-5">
-                <div class="p-4">
+            <div class="col-6 me-3 me-sm-3 p-4">
+                <div class="mb-3 ">
                     <img src="https://picsum.photos/id/{{ $product->id + 10 }}/1920/1080" alt="{{ $product->title }}">
 
                 </div>
+                <div class="m-3 d-lg-none">
+                    <h4>Descrizione del prodotto</h4>
+                    <p>{{ $product->description }}</p>
+                </div>
             </div>
             {{-- titolo, prezzo, aggiungi al carrello, btn modifica --}}
-            <div class="col-4 d-flex justify-content-center align-items-center">
+            <div class="col-6 d-flex justify-content-center align-items-center p-4">
                 <div class="mb-5">
                     {{-- fare il font diverso per il titolo  --}}
                     <h3 class="text-capitalize m-0">{{ $product->title }}</h3>
-                    @if($product->available)
+                    @if ($product->available)
                         <span class="available">Disponibile</span>
                     @else
                         <span class="not-available">Non Disponibile</span>
@@ -32,25 +36,26 @@
                     <h5 class="mt-3 ">Prezzo: <span>{{ $product->price }}</span> €</h5>
                     <div>
 
-                        <div class="my-5">
+                        <div class="my-5 d-sm-none  d-none d-lg-block">
                             <h4>Descrizione del prodotto</h4>
                             <p>{{ $product->description }}</p>
                         </div>
 
 
-                        <div class="d-flex align-items-center">
-                            <button class="my-btn me-5"><i class="fa-solid fa-plus me-2"></i><i class="fa-solid fa-cart-shopping"></i></button>
+                        <div class="d-flex flex-column flex-md-row align-items-center">
+                            <button class="my-btn  my-3  me-md-3"><i class="fa-solid fa-cart-shopping"></i></button>
                             @if (Auth::check() && Auth::user()->role == 'admin')
-                                <a href="{{route('products.edit',$product->id)}}"><button class="my-btn me-5">Modifica</button></a>
-                                <form action="{{route('products.destroy',$product->id)}}" method="post">
+                                <a href="{{ route('products.edit', $product->id) }}"><button
+                                        class="my-btn  my-3 mx-md-3 ">Modifica</button></a>
+                                <form action="{{ route('products.destroy', $product->id) }}" method="post">
                                     @method('DELETE')
                                     @csrf
-                                    <button class="my-btn" type="submit">Elimina</button>
+                                    <button class="my-btn mt-3 mx-md-3" type="submit">Elimina</button>
                                 </form>
                             @endif
                         </div>
                     </div>
-                    {{-- btn modifica .... aggiungere poi solo se admin? --}}
+
                 </div>
             </div>
         </div>
